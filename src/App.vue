dd3d8a66616b8c50e699ed08800c8c99bef057bb
Marcel Baur <marcel.baur@tawny.ai>
2021-04-10 19:02:16 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="toggle" @click="toggleTheme">{{isDark? "💡" : "🌑"}}</div>
+
+  <div class="container">
+  <img alt="MB" src="./assets/Logo-clear.svg" />
+  <Main msg="I'm Marcel Baur" />
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Main from './components/Main.vue'
+import themeChanger from './theme';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Main
+  },
+  data() {
+    return {
+      themeChanger: new themeChanger(),
+      isDark: false,
+    }
+  },
+  created() {
+    this.themeChanger = new themeChanger();
+  },
+  methods: {
+    toggleTheme() {
+      this.themeChanger._darkThemeSwitch();
+      this.isDark = !this.isDark;
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+img {
+  max-height: 360px;
+  max-width: 360px;
+}
+.toggle {
+  cursor: pointer;
+  position: absolute;
+  top: 12px;
+  right: 12px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Work:
+    <a href="https://www.tawny.ai/" target="_blank">TAWNY GmbH</a> <br />
+    Study:
+    <a
+      href="https://www.ifi.lmu.de"
+      target="_blank"
+    >Computer Science at University of Munich</a>
+    
+  </p>
+
+  <p>Refer to my <a href="https://www.github.com/marcel-baur">GitHub</a> for personal projects.</p>
+</template>
+
+<script lang="ts">
+  import themeChanger from '../theme';
+  import { ref, defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+      msg: {
+        type: String,
+        required: true
+      },
+    },
+    data() {
+      return {
+        message: "You opened the component at " + new Date().toLocaleDateString(),
+        counter: 0,
+        themeChanger: new themeChanger(),
+      }
+    },
+    created() {
+      this.themeChanger = new themeChanger();
+    },
+    setup: () => {
+      const count = ref(0)
+      return { count }
+    },
+    methods: {
+      switchTheme() {
+        this.themeChanger._darkThemeSwitch();
+      }, 
+      click(event: any) {
+        this.message = "Changed the component at " + new Date().toLocaleDateString();
+        this.counter++;
+      }
+    }
+  })
+</script>
+
+<style scoped>
+h2 {
+  color: #42b983;
+}
+
+a {
+  color: #f98125
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
